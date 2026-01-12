4de419210f8ebf02bab1e04d6d6024a2ea8860aa
Chaisit91 <Jorvinbasla@gmail.com>
2026-01-12 15:10:54 +0700

    week1-ce385

---
+++ week1/wh1.2.js
@@ -0,0 +1,14 @@
+let message = "Hello World";
+let score = 10;
+let isPassed = true;
+let empty = null;
+let notDefined;
+
+console.log(`myString : ${message} - type ${typeof message}`); 
+console.log(`myNumber : ${score} - type ${typeof score}`); 
+console.log(`myBoolean : ${isPassed} - type ${typeof isPassed}`); 
+console.log(`myNull : ${empty} - type ${typeof empty}`); 
+console.log(`myUndefined : ${notDefined} - type ${typeof notDefined}`); 
+
+
+// ตั้งตัวแปล รับ ชื่อ ตัวเลข boolean Null undefined 
---
+++ week1/wh1.3.js
@@ -0,0 +1,12 @@
+let math = 85 ;
+let sci = 72 ;
+let eng = 90;
+
+let sum = math + sci + eng ;
+let avg = sum/3
+let avg1 = sum >= 80
+
+
+console.log("คะแนนรวม : " , sum );
+console.log("คะแนนเฉลี่ย : " , avg );
+console.log("คะแนนเฉลี่ย >= 80 : " , avg1 );
